0fb5b8c21a46e5e3b9cfed10cfb856f543837e52
Marcelo Ferreira Mota Júnior <marcelo.mota.088@ufrn.edu.br>
2023-01-02 10:40:50 -0300

    @mfmjr

---
+++ DESAFIO 02 - NODE FARM USING JSON/index.js
@@ -0,0 +1,73 @@
+// Acess the functions of reading and recording of dates in 'fs'
+
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate"); // Importing the module
+const slugify = require("slugify"); // Importing the module name address
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+// console.log("dataObj: ", dataObj);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+// console.log("slugs: ", slugs);
+
+const server = http.createServer((req, res) => {
+  // console.log(req.url); // product?id=0
+  const { query, pathname } = url.parse(req.url, true); // ?id=0
+
+  const pathName = req.url; // When user write address
+
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHTML = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHTML);
+    res.end(output);
+  }
+
+  // Product page
+  else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id]; // Json is array of objects
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  }
+
+  // API page
+  else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  }
+
+  // Page not found
+  else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+//           (PORT, SUBANDRESS (HOST) )
+server.listen(8000, "127.0.0.1", () => {
+  console.log(`Listening to request on port ${8000}`);
+});
